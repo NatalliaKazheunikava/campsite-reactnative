1a069f6037436e3118052c64339899d64365b15f
NatalliaK <natalliak.dev@gmail.com>
2022-03-04 16:45:02 -0800

    Added loading and error messages

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -3,6 +3,7 @@ import { ScrollView, Text, FlatList } from 'react-native';
 import { Card, ListItem } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
+import Loading from './LoadingComponent';
 
 const mapStateToProps = state => {
     return {
@@ -28,7 +29,6 @@ class About extends Component {
     }
 
     render() {
-
         const renderPartner = ({item}) => {
             return (
                 <ListItem
@@ -39,7 +39,29 @@ class About extends Component {
             );
         };
 
-        return(
+        if (this.props.partners.isLoading) {
+            return (
+                <ScrollView>
+                    <Mission />
+                    <Card
+                        title='Community Partners'>
+                        <Loading />
+                    </Card>
+                </ScrollView>
+            );
+        }
+        if (this.props.partners.errMess) {
+            return (
+                <ScrollView>
+                    <Mission />
+                    <Card
+                        title='Community Partners'>
+                        <Text>{this.props.partners.errMess}</Text>
+                    </Card>
+                </ScrollView>
+            );
+        }
+        return (
             <ScrollView>
                 <Mission />
                 <Card title="Community Partners">
